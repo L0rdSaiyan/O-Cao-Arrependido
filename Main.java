dcfb3a27999ad0d0a42e06f2d3b5e02c27096344
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-02-13 11:27:11 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		System.out.println("O cão arrependido.");
+		
+		try{
+		    
+		    Thread.sleep(1000);
+		    
+		   }catch(InterruptedException goku){
+		  
+		   }
+		
+		for(int i = 1 ; i < 44; i++){
+		    
+		    System.out.println("Volta o cão arrependido, com suas orelhas tão fartas, com seu osso ruído e com o rabo entre as patas;");
+		    
+		    try{
+		    
+		    Thread.sleep(1000);
+		    
+		   }catch(InterruptedException goku){
+		    
+	       }
+
+	    if(i>2){
+		       
+		        break;
+		    }
+		    
+		}
+		
+		System.out.println("\nSeu Madruga: Muito bem Chaves :clap:, muito bem, muito bem. :clap:");
+		
+		   try{
+		    
+		    Thread.sleep(1000);
+		    
+		    }catch(InterruptedException goku){
+		    
+		    }
+
+		
+		System.out.println("\nChaves: O verso é repetido 44 vezes.");
+		
+		   try{
+		    
+		    Thread.sleep(1000);
+		    
+     		}catch(InterruptedException goku){
+		    
+	     	}
+
+		
+		System.out.println("\n44 uma ova, sai daqui vai.");
+		
+	}
+}
